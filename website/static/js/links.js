86899adc01729118cecfb59ccf27d8e82ce33433
Erwan Carriou <erwan.carriou@mac.com>
2018-07-17 19:30:15 +0200

    fix link

--- website/static/js/links.js
+++ website/static/js/links.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', function () {
-  document.querySelector('header > a').setAttribute('href', '../docs/what-is-system-designer.html');
+  document.querySelector('header > a').setAttribute('href', '../docs/what-is-system-runtime.html');
 });
